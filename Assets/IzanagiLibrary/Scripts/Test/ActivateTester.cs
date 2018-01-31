abe79ce0fd5365c269ccfe1ec8848a8b078913f5
Go Sato <g-sato@colopl.co.jp>
2018-01-31 18:08:50 +0900

    シェーダーのプロパティを等速で変化させるスクリプト実装

---
+++ Assets/IzanagiLibrary/Scripts/Test/ActivateTester.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using IzanagiLibrary;
+
+public class ActivateTester : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject _targetObject;
+    [SerializeField]
+    private ShaderPropertyChanger _changer;
+    [SerializeField]
+    private float _startValue = 0.0f;
+    [SerializeField]
+    private float _goalValue = 2.0f;
+    [SerializeField]
+    private float _duration = 2.0f;
+
+    private bool _isActivated = false;
+
+    private void Start()
+    {
+        if (_targetObject.activeInHierarchy)
+        {
+            _targetObject.SetActive(false);
+        }
+        _isActivated = false;
+    }
+
+    private void Update()
+    {
+        // デバッグ用
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            ActivateTest();
+        }
+        else if (Input.GetKeyDown(KeyCode.D))
+        {
+            DeactivateTest();
+        }
+    }
+
+    [ContextMenu("ActivateTest")]
+    public void ActivateTest()
+    {
+        if (_isActivated)
+        {
+            return;
+        }
+
+        _targetObject.SetActive(true);
+
+        _changer.OnFinish = () =>
+        {
+            _isActivated = true;
+        };
+
+        _changer.ChangeProperty(_startValue, _goalValue, _duration);
+    }
+
+    [ContextMenu("DeactivateTest")]
+    public void DeactivateTest()
+    {
+        if (!_isActivated)
+        {
+            return;
+        }
+
+        _changer.OnFinish = () =>
+        {
+            _targetObject.SetActive(false);
+            _isActivated = false;
+        };
+
+        _changer.ChangeProperty(_goalValue, _startValue, _duration);
+    }
+}
